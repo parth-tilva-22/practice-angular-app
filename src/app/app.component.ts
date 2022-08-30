3ed96c7ac8334d0147b55027b5c80d0656595f77
MRISOFTWARE\Parth.Tilva <Parth.Tilva@mrisoftware.com>
2022-08-30 14:41:35 +0530

    side bar working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice-angular-app';
+  isOpen: boolean = false
+  onToggle():void{
+    this.isOpen = !this.isOpen;
+  }
 }
